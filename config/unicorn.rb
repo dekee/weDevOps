348e81e2f3d3d842ae100c7de7ff501dad9fcca8
Derrick Johnson <derrick.johnson@hp.com>
2013-06-16 22:09:53 -0400

    removed blog references

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,9 @@
-unicorn.rbroot = "/home/deployer/apps/blog/current"
+unicorn.rbroot = "/home/deployer/apps/weDevOps/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
-stderr_path "#{root}/log/unicorn.log"
-stdout_path "#{root}/log/unicorn.log"
+stderr_path "#{root}/log/weDevOps.log"
+stdout_path "#{root}/log/weDevOps.log"
 
-listen "/tmp/unicorn.blog.sock"
+listen "/tmp/unicorn.weDevOps.sock"
 worker_processes 2
 timeout 30
