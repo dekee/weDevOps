1eb3c21930b71dc4e43c857b7d9f7fef8d890ef6
Derrick Johnson <derrick.johnson@hp.com>
2013-06-16 20:08:13 -0400

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeDevOps::Application.config.secret_token = 'dd5d238c0f9748800af2f29c67d38f0c9ed6d7c325c75f8fae79ba200b8fe8ad5cd296a45acb88741164d16e978cf813ff033e0da91668164788f7cf5f0f544c'
