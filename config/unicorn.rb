ae797744b5faa7cb56fa7524c9ddddc531965537
Derrick Johnson <derrick.johnson@hp.com>
2013-06-16 22:41:44 -0400

    fixed unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,9 @@
 unicorn.rbroot = "/home/deployer/apps/weDevOps/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
-stderr_path "#{root}/log/weDevOps.log"
-stdout_path "#{root}/log/weDevOps.log"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.weDevOps.sock"
+listen "/tmp/unicorn.wedevops.sock"
 worker_processes 2
 timeout 30
